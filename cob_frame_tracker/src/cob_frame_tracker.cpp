22695e85f62d90675d78fe851e2b7a7aeb2127e5
ipa-fxm <felix.messmer@ipa.fhg.de>
2014-11-01 11:07:41 +0100

    temporarily revert to non-feedforward pid_controllers

--- cob_frame_tracker/src/cob_frame_tracker.cpp
+++ cob_frame_tracker/src/cob_frame_tracker.cpp
@@ -131,13 +131,16 @@ void CobFrameTracker::publish_twist(ros::Duration period)
 	
 	if(movable_trans_)
 	{
-		twist_msg.linear.x = pid_controller_trans_x_.computeCommand(transform_msg.transform.translation.x,last_err_x_/period.toSec(), period);
-		twist_msg.linear.y = pid_controller_trans_y_.computeCommand(transform_msg.transform.translation.y,last_err_y_/period.toSec(), period);
-		twist_msg.linear.z = pid_controller_trans_z_.computeCommand(transform_msg.transform.translation.z,last_err_z_/period.toSec(), period);
-		last_err_x_ = transform_msg.transform.translation.x;
-		last_err_y_ = transform_msg.transform.translation.y;
-		last_err_z_ = transform_msg.transform.translation.z;
+		twist_msg.linear.x = pid_controller_trans_x_.computeCommand(transform_msg.transform.translation.x, period);
+		twist_msg.linear.y = pid_controller_trans_y_.computeCommand(transform_msg.transform.translation.y, period);
+		twist_msg.linear.z = pid_controller_trans_z_.computeCommand(transform_msg.transform.translation.z, period);
 		
+		//twist_msg.linear.x = pid_controller_trans_x_.computeCommand(transform_msg.transform.translation.x,last_err_x_/period.toSec(), period);
+		//twist_msg.linear.y = pid_controller_trans_y_.computeCommand(transform_msg.transform.translation.y,last_err_y_/period.toSec(), period);
+		//twist_msg.linear.z = pid_controller_trans_z_.computeCommand(transform_msg.transform.translation.z,last_err_z_/period.toSec(), period);
+		//last_err_x_ = transform_msg.transform.translation.x;
+		//last_err_y_ = transform_msg.transform.translation.y;
+		//last_err_z_ = transform_msg.transform.translation.z;
 	}
 	
 	if(movable_rot_)
