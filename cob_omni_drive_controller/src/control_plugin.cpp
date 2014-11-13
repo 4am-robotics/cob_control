83f41062934a1c68f5620dd236fb3e0732b80cd0
Florian Weisshardt <florian.weisshardt@ipa.fraunhofer.de>
2014-11-13 16:27:06 +0100

    migrated to shared implementation of GeomController

--- cob_omni_drive_controller/src/control_plugin.cpp
+++ cob_omni_drive_controller/src/control_plugin.cpp
@@ -11,75 +11,11 @@
 #include <boost/weak_ptr.hpp>
 #include <boost/thread/mutex.hpp>
 
+#include "GeomController.h"
+
 namespace cob_omni_drive_controller
 {
 
-template<typename T> class GeomController: public controller_interface::Controller<T> {
-protected:
-    std::vector<hardware_interface::JointHandle> steer_joints_, drive_joints_;
-    std::vector<UndercarriageCtrlGeom::WheelState> wheel_states_;
-    boost::scoped_ptr<UndercarriageCtrlGeom> geom_;
-public:    
-    virtual bool init(hardware_interface::VelocityJointInterface* hw, ros::NodeHandle &root_nh, ros::NodeHandle& controller_nh){
-
-        std::vector<std::string> steer_names, drive_names;
-
-        if (!controller_nh.getParam("steer_joints", steer_names)){
-            ROS_ERROR("Parameter 'steer_joints' not set");
-            return false;
-        }
-        if (!controller_nh.getParam("drives_joints", drive_names)){
-            ROS_ERROR("Parameter 'drives_joints' not set");
-            return false;
-        }
-
-        if (steer_names.size()!=drive_names.size()){
-            ROS_ERROR("Number of steer joints does not match number of drive joints");
-            return false;
-        }
-
-        if (drive_names.size() < 3){
-            ROS_ERROR("At least three wheel are needed.");
-            return false;
-        }
-
-        for (unsigned i=0; i<steer_names.size(); i++){
-            steer_joints_.push_back(hw->getHandle(steer_names[i]));
-            drive_joints_.push_back(hw->getHandle(drive_names[i]));
-        }
-        wheel_states_.resize(steer_names.size());
-
-        std::vector<UndercarriageCtrlGeom::WheelParams> params;
-
-        std::string ini_directory;
-        if (!controller_nh.getParam("ini_directory", ini_directory)){
-            ROS_ERROR("Parameter 'ini_directory' not set");
-            return false;
-        }
-
-        try{
-            UndercarriageCtrlGeom::parseIniFiles(params, ini_directory);
-        }
-        catch(...){
-            ROS_ERROR("INI file parsing failed");
-            return false;
-        }
-        geom_.reset(new UndercarriageCtrlGeom(params));
-        return true;
-    }
-
-    virtual void update(const ros::Time& time, const ros::Duration& period){
-
-        for (unsigned i=0; i<wheel_states_.size(); i++){
-            wheel_states_[i].dAngGearSteerRad = steer_joints_[i].getPosition();
-            wheel_states_[i].dVelGearSteerRadS = steer_joints_[i].getVelocity();
-            wheel_states_[i].dVelGearDriveRadS = drive_joints_[i].getVelocity();
-        }
-        geom_->updateWheelStates(wheel_states_);
-    }    
-};
-
-// this controller gets access to the JointStateInterface
 class WheelController: public GeomController<hardware_interface::VelocityJointInterface>
 {
 public:
@@ -87,7 +23,7 @@ public:
 
     virtual bool init(hardware_interface::VelocityJointInterface* hw, ros::NodeHandle &root_nh, ros::NodeHandle& controller_nh){
 
-        if(!GeomController::init(hw,root_nh, controller_nh)) return true;
+        if(!GeomController::init(hw, controller_nh)) return true;
 
         wheel_commands_.resize(wheel_states_.size());
         twist_subscriber_ = controller_nh.subscribe("command", 1, &WheelController::topicCallbackTwistCmd, this);
@@ -100,7 +36,7 @@ public:
     }
     virtual void update(const ros::Time& time, const ros::Duration& period){
 
-        GeomController::update(time, period);
+        GeomController::update();
 
         boost::shared_ptr<const geometry_msgs::Twist> target;
         {
