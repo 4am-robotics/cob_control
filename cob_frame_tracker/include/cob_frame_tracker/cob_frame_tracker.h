a0f6553b1b443f8338366edda0a90cbb74f15cbd
Christian Ehrmann <christian.ehrmann@ipa.fraunhofer.de>
2015-01-14 11:13:50 +0100

    frame_tracker_new

--- cob_frame_tracker/include/cob_frame_tracker/cob_frame_tracker.h
+++ cob_frame_tracker/include/cob_frame_tracker/cob_frame_tracker.h
@@ -44,27 +44,112 @@
 #include <kdl/frames.hpp>
 #include <kdl_parser/kdl_parser.hpp>
 
+struct CobFrameTrackerParams {
+	int timer_value;
+	double distance_threshold;
+};
+
+
 class CobFrameTracker
 {
 public:
-	CobFrameTracker() {;}
+	CobFrameTracker(std::string name):
+	as_(nh_, name, false),
+	timer_value_(10),
+	distance_threshold_(0.005)
+	{
+		as_.registerGoalCallback(boost::bind(&FrameTrackingAction::goalCB, this));
+		as_.registerPreemptCallback(boost::bind(&FrameTrackingAction::preemptCB, this));
+		as_.start();
+	}
 	~CobFrameTracker();
 	
 	bool initialize();
 	void run();
+	void goalCB();
+	void preemptCB();
+	void succeed();
+	void abort();
+	bool searchForAbortionCriteria();
 	
 	void publish_twist(ros::Duration period);
 	bool start_tracking_cb(cob_srvs::SetString::Request& request, cob_srvs::SetString::Response& response);
 	bool stop_tracking_cb(std_srvs::Empty::Request& request, std_srvs::Empty::Response& response);
 	
-	ros::NodeHandle nh_;
+	void jointstate_cb(const sensor_msgs::JointState::ConstPtr& msg);
+	void updateList(KDL::Twist twist,  double time_filter_threshold);
+	void updateList2(double norm, double time_filter_threshold);
+	void calcMean();
+	double meanNorm();
+
+	void publish_twist(ros::Duration period);
+	//reconfigure params
+	boost::recursive_mutex reconfig_mutex_;
+	boost::shared_ptr< dynamic_reconfigure::Server<cob_frame_tracker::FrameTrackingActionConfig> > reconfigure_server_;
+	void reconfigure_callback(cob_frame_tracker::FrameTrackingActionConfig &config, uint32_t level);
+	void SetFrameTrackingActionParams(FrameTrackingActionParams params){params_ = params;}
 	tf::TransformListener tf_listener_;
+
+protected:
+
+	ros::NodeHandle nh_;
+	std::vector<std::string> joints_;
+	unsigned int dof_;
+	std::string action_name_;
+	actionlib::SimpleActionServer<cob_frame_tracker::FrameTrackingAction> as_;
+	cob_tracking_action::TrackingFeedback feedback_;
+	cob_tracking_action::TrackingResult result_;
 	
+	//goal definition:
+	std::string target_tracking_frame_;
+	bool reachable_goal_;
+	int tracking_time_;
+	ros::Time start_of_tracking_;
+
+//my calcs begin:
+	double eukl_dist_lin_;
+	//my cals end
+
+	//KDL:
+	/// KDL Conversion
+	KDL::Chain chain_;
+	KDL::JntArray q_temp;
+	KDL::JntArray q_dot_temp;
+	KDL::JntArray last_q_;
+	KDL::JntArray last_q_dot_;
+//	std::vector<std::string> joints_;
+	KDL::ChainFkSolverVel_recursive* jntToCartSolver_vel_;
+//	KDL::ChainFkSolverVel_recursive* p_fksolver_vel_;
+//	KDL::ChainIkSolverVel_pinv* p_iksolver_vel_;
+//	unsigned int dof_;
+	KDL::Vector vector_vel_,vector_rot_;
+	std::string chain_base_;
+	std::string chain_tip_;
+	KDL::Tree my_tree;
+	std::string robot_desc_string;
+
+	TrackingActionParams params_;
+	int timer_value_;
+	double distance_threshold_;
+
+	ros::Time timer;
+
+	/// Abortion Criteria
+	double cart_distance_;
+	double rot_distance_;
+	KDL::Twist current_twist_;
+	KDL::Twist target_twist_;
+	KDL::Twist diff_twist_;
+	/// Helping vars:
+	std::string abortion_message_;
+	std::vector<std::vector<double> > list_of_twists_stamped_;
+	std::vector<std::vector<double> > list_of_norms_stamped_;
+
 private:
 	double update_rate_;
 	
 	bool tracking_;
-	std::string tracking_frame_;	//goal frame
+//	std::string tracking_frame_;	//goal frame
 	std::string active_frame_;		//twists with respect to this frame
 	std::string base_link_;
 	
