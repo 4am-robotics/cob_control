75155e2ed8c268839a02a84faba399f0da73c416
Mathias Lüdtke <mathias.luedtke@ipa.fraunhofer.de>
2017-02-14 16:17:16 +0100

    added PosCtrlParams

--- cob_omni_drive_controller/include/cob_omni_drive_controller/UndercarriageCtrlGeom.h
+++ cob_omni_drive_controller/include/cob_omni_drive_controller/UndercarriageCtrlGeom.h
@@ -201,12 +201,7 @@ private:
 
 class UndercarriageCtrl : public UndercarriageGeomBase {
 public:
-    struct CtrlParams{
-        double dWheelNeutralPos;
-
-        double dMaxDriveRateRadpS;
-        double dMaxSteerRateRadpS;
-
+    struct PosCtrlParams {
         /** ------- Position Controller Steer Wheels -------
         * Impedance-Ctrlr Prms
         *  -> model Stiffness via Spring-Damper-Modell
@@ -218,6 +213,15 @@ public:
         *  m_dDDPhiMax maximum angular acceleration (cut-off)
         */
         double dSpring, dDamp, dVirtM, dDPhiMax, dDDPhiMax;
+    };
+    
+    struct CtrlParams{
+        double dWheelNeutralPos;
+
+        double dMaxDriveRateRadpS;
+        double dMaxSteerRateRadpS;
+        
+        PosCtrlParams pos_ctrl;
     };
 
     struct WheelParams {
