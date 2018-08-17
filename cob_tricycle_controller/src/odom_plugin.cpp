6124f2d988080e4c6531643e24f7c6f9c645ec92
fmessmer <felixmessmer@gmail.com>
2018-08-17 10:33:36 +0200

    fix forward kinematics in odom_plugin

--- cob_tricycle_controller/src/odom_plugin.cpp
+++ cob_tricycle_controller/src/odom_plugin.cpp
@@ -194,11 +194,11 @@ private:
         wheel_state_.drive_pos = drive_joint_.getPosition();
         wheel_state_.drive_vel = drive_joint_.getVelocity();
 
-        ///ToDo:
         //calculate forward kinematics
-        platform_state_.velX = 2*M_PI*wheel_state_.radius*wheel_state_.drive_vel;
+        double v_wheel = 2*M_PI*wheel_state_.radius*wheel_state_.drive_vel;
+        platform_state_.velX = v_wheel * cos(wheel_state_.steer_pos);
         platform_state_.velY = 0.0;
-        platform_state_.rotTheta = 0.0;
+        platform_state_.rotTheta = (v_wheel/3*M_PI*wheel_state_.pos_x)*sin(wheel_state_.steer_pos);
     }
 };
 
