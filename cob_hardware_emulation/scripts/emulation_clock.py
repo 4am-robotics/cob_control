ee72f6d966337f77eb216c12ebe3beaf04dbde74
fmessmer <felixmessmer@gmail.com>
2020-02-14 11:58:35 +0100

    implement emulation_clock with timer callback

--- cob_hardware_emulation/scripts/emulation_clock.py
+++ cob_hardware_emulation/scripts/emulation_clock.py
@@ -4,18 +4,25 @@ import time
 import rospy
 from rosgraph_msgs.msg import Clock
 
-rospy.init_node('emulation_clock', disable_rostime=True)
-pub = rospy.Publisher('/clock', Clock, queue_size=1)
-time_factor = rospy.get_param('/emulation_time_factor', 1.0)
-if not time_factor > 0.0:
-    rospy.logerr("emulation_time_factor must be >0.0, but is {}. exiting...".format(time_factor))
-    exit(-1)
+class EmulationClock(object):
+    def __init__(self):
+        self.t = time.time()
+        self.dt = 0.01
+        self.time_factor = rospy.get_param('/emulation_time_factor', 1.0)
+        if not self.time_factor > 0.0:
+            rospy.logerr("emulation_time_factor must be >0.0, but is {}. exiting...".format(self.time_factor))
+            exit(-1)
+        self.pub = rospy.Publisher('/clock', Clock, queue_size=1)
+        rospy.Timer(rospy.Duration(self.dt), self.timer_cb)
 
-t = time.time()
-dt = 0.01
-while not rospy.is_shutdown():
-    t+=dt*time_factor
-    msg = Clock()
-    msg.clock = rospy.Time(t)
-    pub.publish(msg)
-    time.sleep(dt)
+    def timer_cb(self, event):
+        self.t+=self.dt*self.time_factor
+        msg = Clock()
+        msg.clock = rospy.Time(self.t)
+        self.pub.publish(msg)
+
+
+if __name__ == '__main__':
+    rospy.init_node('emulation_clock', disable_rostime=True)
+    EmulationClock()
+    rospy.spin()
